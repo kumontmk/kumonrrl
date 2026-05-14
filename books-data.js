58c35621782dfb8672f9a745fb6f7a0a0c1ebb0d
kumontmk <kumontmk@gmail.com>
2026-05-14 12:03:44 +0800

    Update book entry in books-data.js

--- books-data.js
+++ books-data.js
@@ -1,14 +1,16 @@
+// books-data.js
 const libraryBooks = [
-    {
-        id: 1,
-        title: "The Cat in the Hat",
-        author: "Dr. Seuss",
-        genre: "Picture Book",
-        location: "Shelf A1",
-        rrlLevel: "A",
-        coverImage: null,
-        status: "available",
-        borrower: null,
-        borrowDate: null
-    }
+  {
+    id: 1,
+    title: "Sample Book",
+    author: "Author Name",
+    genre: "Fiction",
+    location: "Shelf A1",
+    rrlLevel: "A",
+    coverImage: null,
+    status: "available",
+    borrower: null,
+    borrowDate: null
+  },
+  // Add more books here...
 ];

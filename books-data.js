12ab5291b5eb2d6976a26d12fcfa570c3c42fab2
kumontmk <kumontmk@gmail.com>
2026-05-14 11:56:58 +0800

    Add initial book entry to libraryBooks
    Added a book object to the libraryBooks array.

--- books-data.js
+++ books-data.js
@@ -1,7 +1,14 @@
 const libraryBooks = [
+    {
+        id: 1,
+        title: "The Cat in the Hat",
+        author: "Dr. Seuss",
+        genre: "Picture Book",
+        location: "Shelf A1",
+        rrlLevel: "A",
+        coverImage: null,
+        status: "available",
+        borrower: null,
+        borrowDate: null
+    }
 ];
-
-// Export for use in other files
-if (typeof module !== 'undefined' && module.exports) {
-  module.exports = libraryBooks;
-}
